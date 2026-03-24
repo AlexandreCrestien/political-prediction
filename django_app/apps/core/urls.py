89b9f9e5c80e889408ca9942be8e908e48bfa06e
Alexandre Crestien <alexandre.crestien@email.com>
2026-03-24 09:52:47 +0100

    fix: deleate django_app

--- django_app/apps/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AccueilView
-
-urlpatterns = [
-    path('home/', AccueilView.as_view(), name='home'),
-]
--- django_app/apps/predictions/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PredictionsView
-
-urlpatterns = [
-    path('predictions/', PredictionsView.as_view(), name='predictions'),
-]
