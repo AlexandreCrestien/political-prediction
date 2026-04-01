4a7af60923c77d73cacab9dc10c8298f6a4b4839
Souhaib <massroursouhaib@hotmail.com>
2026-04-01 15:49:11 +0200

    rework login and signup front

--- django_political_app/detail/views.py
+++ django_political_app/detail/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .services import DetailService
 import math
 
 # Create your views here.
-class DetailView(TemplateView):
+class DetailView(LoginRequiredMixin,TemplateView):
     template_name = 'detail.html'
 
     def get_context_data(self, **kwargs):
